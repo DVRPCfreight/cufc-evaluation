411bdf38a7cfaf670582616bd3e5bda8688020ab
DVRPCfreight <mruane@dvrpc.org>
2017-01-23 11:56:05 -0500

    feature layer update

--- config/defaults.js
+++ config/defaults.js
@@ -33,7 +33,7 @@ define({
     "displayText": "<b>Critical Urban Freight Corridor Feedback</b><p>Help us verify the freight network in the Pennsylvania counties of the DVRPC region.</p><p>Give them info about that ish.</p>",
     "color": "#206bdb",
     "featureLayer": {
-        "id": "LandUseCasesVotesComments_8488",
+        "id": "CUFC_vote_comments",
         "fields": [{
             "id": "sortField",
             "fields": ["CASENAME"]
